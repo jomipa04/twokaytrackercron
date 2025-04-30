54e24a763970eb943db590e40a9e28234dfed251
jomipa04 <82591456+jomipa04@users.noreply.github.com>
2025-04-30 13:36:31 +0800

    added port

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
 const url = `https://twokaybackend.onrender.com/api/`;
 const interval = 780000; // Interval in milliseconds (13 min)
 
@@ -5,7 +17,7 @@ function reloadWebsite() {
   fetch(url)
     .then((response) => {
       console.log(
-        `Reloaded at ${new Date().toISOString()}: Status Code ${
+        `Reloaded 2k backend at ${new Date().toISOString()}: Status Code ${
           response.status
         }`
       );
@@ -17,4 +29,23 @@ function reloadWebsite() {
       );
     });
 }
+
+function reloadThis() {
+  fetch("https://twokaytrackercron.onrender.com")
+    .then((response) => {
+      console.log(
+        `Reloaded cron job at ${new Date().toISOString()}: Status Code ${
+          response.status
+        }`
+      );
+    })
+    .catch((error) => {
+      console.error(
+        `Error reloading at ${new Date().toISOString()}:`,
+        error.message
+      );
+    });
+}
+
+setInterval(reloadThis, interval);
 setInterval(reloadWebsite, interval);
